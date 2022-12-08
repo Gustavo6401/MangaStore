c79bcf61146acd24321e72c967cd652a897beeef
Gustavo6401 <gsoey2019123@gmail.com>
2022-12-08 14:15:14 -0300

    Usuário Cadastrado já pode cadastrar novo endereço

---
+++ MangaStore/MangaStore.Web/Controllers/EnderecoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MangaStore.Web.Models;
+using MangaStore.Web.Repositories;
+
+namespace MangaStore.Web.Controllers
+{
+    public class EnderecoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Cliente")]
+        public IActionResult Create(int idCliente)
+        {
+            EnderecoCliente endereco = new EnderecoCliente();
+            endereco.ClienteId = idCliente;
+
+            return View(endereco);
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Id,CEP,Rua,Bairro,Cidade,UF,Numero,EnderecoPadrao,Cliente,ClienteId")] EnderecoCliente endereco)
+        {
+            EnderecoClienteRepository repository = new EnderecoClienteRepository();
+            repository.Add(endereco);
+
+            return RedirectToAction("MeuCadastro", "Cliente");
+        }
+    }
+}
---
+++ MangaStore/MangaStore.Web/Controllers/StatusController.cs
@@ -0,0 +1,26 @@
+﻿using MangaStore.Web.Models;
+using MangaStore.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangaStore.Web.Controllers
+{
+    public class StatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Edit(string statusPedido, int pedidoId)
+        {
+            StatusRepository repository = new StatusRepository();
+
+            StatusPedido model = repository.GetByPedidoId(pedidoId);
+            model.Status = statusPedido;
+
+            repository.Update(model);
+
+            return RedirectToAction("Index", "Pedido");
+        }
+    }
+}
